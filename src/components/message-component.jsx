2d12209e9fac1975a27ca839334b25a616f55b05
gracedward <edward.chung.designer@gmail.com>
2022-05-28 00:55:10 +0100

    fixed makes the dependencies of useEffect Hook issue with useCallback

--- src/components/message-component.jsx
+++ src/components/message-component.jsx
@@ -1,16 +1,16 @@
-import { useContext, useEffect } from 'react';
+import { useCallback, useContext, useEffect } from 'react';
 import { MessageContext } from "../contexts/message-context";
 
 const Message = () => {
     const { message, setMessage, showMessage, setShowMessage } = useContext(MessageContext);
-    const clearMessage = () => {
+    const clearMessage = useCallback(() => {
         setShowMessage(false);
-    }
+    }, [])
 
     useEffect(() => {
         const timer = setTimeout(() => clearMessage(), 2000);
         return () => clearTimeout(timer);
-    }, [message, clearMessage]); // to clearTimeout in case of multiple messages being fired in a short time
+    }, [clearMessage, message]); // to clearTimeout in case of multiple messages being fired in a short time
 
     return (
          <div className={`fixed p-4 bg-orange-100 z-50 left-[50%] -ml-[10rem] w-[20rem] transition-all -translate-y-20 shadow ${showMessage?'translate-y-4':''}`} onClick={clearMessage}>{message}</div>
