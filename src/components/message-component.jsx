91881a3fb5e1c7a9527653aed8dcb89110b5f9fb
gracedward <edward.chung.designer@gmail.com>
2022-05-28 01:02:09 +0100

    bug fixed

--- src/components/message-component.jsx
+++ src/components/message-component.jsx
@@ -1,16 +1,16 @@
-import { useCallback, useContext, useEffect } from 'react';
+import { useContext, useEffect } from 'react';
 import { MessageContext } from "../contexts/message-context";
 
 const Message = () => {
     const { message, setMessage, showMessage, setShowMessage } = useContext(MessageContext);
-    const clearMessage = useCallback(() => {
+    const clearMessage = () => {
         setShowMessage(false);
-    }, [])
+    }
 
     useEffect(() => {
         const timer = setTimeout(() => clearMessage(), 2000);
         return () => clearTimeout(timer);
-    }, [clearMessage, message]); // to clearTimeout in case of multiple messages being fired in a short time
+    }, [message, clearMessage]); // to clearTimeout in case of multiple messages being fired in a short time
 
     return (
          <div className={`fixed p-4 bg-orange-100 z-50 left-[50%] -ml-[10rem] w-[20rem] transition-all -translate-y-20 shadow ${showMessage?'translate-y-4':''}`} onClick={clearMessage}>{message}</div>
